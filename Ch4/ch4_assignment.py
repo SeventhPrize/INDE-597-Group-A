c710f0d0ad07e7a81f633cba01e7edb1fdf7a98d
George Lyu <georgemylyu@gmail.com>
2024-02-02 13:32:38 -0600

    finished policy iteration

--- Ch4/ch4_assignment.py
+++ Ch4/ch4_assignment.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import random
 import numpy as np
 
 class Game(ABC):
@@ -126,22 +127,106 @@ class DynamicProgram:
                 value[st] = 0
                 
                 # Sum over actions
-                for act in self.game.actions[st]:
+                for act in policy[st].keys():
                     prob_act = policy[st][act]  # probability of selecting the action given the current state
 
                     # Get potential action outcomes
                     state_reward_list, prob_state_reward_list = self.game.get_state_and_reward(st, act)
                     for state_reward, prob_state_reward in zip(state_reward_list, prob_state_reward_list):
 
-                        # Compute contributino to the value function
+                        # Compute contribution to the value function
                         value[st] += prob_act * prob_state_reward * (state_reward[1] + disc * old_value[state_reward[0]])
                 
                 # Compute change in this state's value function
                 delta = max(delta, abs(old_value[st] - value[st]))
-        return value    
+        return value   
+
+    def policy_improvement(self, policy, value, disc=1.0):
+        '''
+        Mutates and improves the given policy
+        Assumes the given policy is deterministic: each state maps to a dictionary containing, as the only key, the best action which maps to 1
+        INPUT
+            policy; dictionary mapping each state to a singleton dictionary, which maps a single action to 1
+            value; evaluation of the given policy as a dictionary mapping each state to its value
+            disc; discount rate
+        RETURNS
+            true if no improvements could be made; else false
+        '''
+        stable = True
+        
+        # For each state, greedily select best action
+        for st in self.game.states:
+
+            # If absorbing state, continue
+            if len(self.game.actions[st]) == 0:
+                continue
+
+            # Record the old action and trackers for new action
+            old_action = list(policy[st].keys())[0]
+            best_action = None
+            best_action_value = float('-inf')
+
+            # For each action, calculate the value of that action
+            for act in self.game.actions[st]:
+                action_value = 0
+                state_reward_list, prob_state_reward_list = self.game.get_state_and_reward(st, act)
+                for state_reward, prob_state_reward in zip(state_reward_list, prob_state_reward_list):
+
+                    # Compute contribution to the value function
+                    action_value += prob_state_reward * (state_reward[1] + disc * value[state_reward[0]])
+
+                # Track if new best action is found
+                if action_value > best_action_value:
+                    best_action = act
+                    best_action_value = action_value      
+            
+            # Update action
+            if old_action != best_action:
+                stable = False                      
+            policy[st] = {best_action: 1}
+
+        return stable
+    
+    def policy_iteration(self, policy, tol=1.0, disc=1.0):
+        '''
+        Executes policy iteration.
+        Given a base policy, repeatedly performs evaluation and improvement until no improvement can be made.
+        Assumes the base policy is deterministic, where each state is mapped only to a singleton dictionary that maps the selected action to probability 1
+        INPUT
+            policy; dictionary mapping each state to a singleton dictionary, which maps a single action to 1
+            tol; optimality tolerance for evaluation
+            disc; discount rate
+        RETURNS 
+            best deterministic policy
+        '''
+        # Copy policy
+        policy = policy.copy()
+
+        # Repeatedly evaluate and improve until no improvement is possible
+        stable = False
+        while not stable:
+            value = self.policy_evaluation(policy, tol, disc)
+            stable = self.policy_improvement(policy, value, disc)
+        return policy
+
+
+            
+
 
 gw = GridWorld()
 dp = DynamicProgram(gw)
+
+# Equiprobable random policy evaluation
 erp = gw.make_equiprobable_random_policy()
 value = dp.policy_evaluation(erp, tol=0)
 print(value)
+
+# Policy iteration
+
+# Construct arbitrary terminable policy: go left until you leftmost column, then go up
+pol = {st : {"L": 1} for st in gw.states}
+for st in [4, 8, 12]:
+    pol[st] = {"U": 1}
+pol[0] = {}
+best_pol = dp.policy_iteration(pol, tol=0)
+print(best_pol)
