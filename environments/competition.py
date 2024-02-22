2c0e7babbb95784e477ff1300a1c191a13c3f586
George Lyu <georgemylyu@gmail.com>
2024-02-22 12:26:21 -0600

    Cliff walking

--- environments/competition.py
+++ environments/competition.py
@@ -64,12 +64,16 @@ class EnvironmentVersus(ABC):
         '''
         return None
             
-    def play_game(self, first_agent_ind:int=-1):
+    def play_game(self, opening:Sequence=None):
         '''
         Plays the game and outputs the episode path
         INPUT
-            first_agent_ind; integer index of the agent to make the first move
-                if -1, randomly selects an agent
+            opening; a 2-tuple with elements
+                0: initial state
+                1: first agent to move
+
+                if default None, then sets initial state using self.reset() and initial agent randomly  
+            
         RETURNS
             episode path; list of 4-tuples, each of which has components:
                 0: state
@@ -79,17 +83,20 @@ class EnvironmentVersus(ABC):
                 The last element is the final state, given as (final_state, None, None, None)
             rewards; 2-length list of each agent's final rewards
         '''
-        # Select (default) agent
-        if first_agent_ind == -1:
+        # Set opening default by reset
+        if opening is None:
+            state = self.reset()
             agent_ind = random.choice(range(len(self.agents)))
+
+        # Set given opening
         else:
-            agent_ind = first_agent_ind
+            (state, agent_ind) = opening
+            self.current_state = state
 
         # Initialize
         episode_path = []
-        individual_paths = [[]] * len(self.agents)
+        individual_paths = [[] for _ in range(len(self.agents))]
         rewards = [0] * len(self.agents)
-        state = self.reset()
 
         # Until done, make steps and record action
         while True:
@@ -103,7 +110,7 @@ class EnvironmentVersus(ABC):
             # Get outcome
             next_state, reward, done, next_agent_ind = self.step(action, agent_ind)
             episode_path.append((state, action, reward, agent_ind))
-            individual_paths[agent_ind].append((self.reinterpret_state_for_agent(state, agent_ind), action, reward))
+            individual_paths[agent_ind].append((reinterpret_state, action, reward))
             rewards[agent_ind] += reward
             state = next_state
             agent_ind = next_agent_ind
@@ -111,16 +118,63 @@ class EnvironmentVersus(ABC):
             # Add termination state
             if done:
                 episode_path.append((state, None, 0, None))
-                for agent_ind in range(len(self.agents)):
-                    individual_paths.append((self.reinterpret_state_for_agent(state, agent_ind), action, 0))
+
+                # Modify each agent's last action based on the end-game rewards
+                for agent_ind_loop in range(len(self.agents)):
+                    if len(individual_paths[agent_ind_loop]) == 0:
+                        continue
+                    last_sar = list(individual_paths[agent_ind_loop][-1])
+                    reward_change = self.compute_game_end_reward(episode_path, agent_ind_loop)
+                    last_sar[2] += reward_change
+                    individual_paths[agent_ind_loop][-1] = tuple(last_sar)
+                    rewards[agent_ind_loop] += reward_change
+
+                # Give each agent the termination state
+                for agent_ind_loop in range(len(self.agents)):
+                    individual_paths[agent_ind_loop].append((self.reinterpret_state_for_agent(state, agent_ind_loop), action, 0))
 
             # Show history to agents
-            for agent_ind, agent in enumerate(self.agents):
-                agent.see_history(individual_paths[agent_ind])
+            for agent_ind_loop, agent in enumerate(self.agents):
+                agent.see_history(individual_paths[agent_ind_loop])
 
             # Terminate
             if done:
                 return episode_path, rewards
+            
+    def compute_game_end_reward(self, history:List, agent_ind:int):
+        '''
+        Computes the reward accrued by the given indexed agent at the end of the game
+        
+        This implementation assumes a zero-sum game: gives the indexed reward equal to the negative of the reward gained by the last action played.
+        This implementation does not indicate changes to the reward of the agent who played the last action, since that agent already got reward from their action.
+        Consider alternate implementations for different games.
+        
+        INPUT
+            history; episode pathway as a list of 4-tuples, each of which has components:
+                0: state
+                1: action
+                2: reward
+                3: index of agent
+                The last element is the final state, given as (final_state, None, None, None)
+            agent_ind; integer index of the agent to compute the game end rewards for
+        RETURNS
+            amount by which to modify the reward of the last action made by the indexed agent
+        '''
+        # Check that the last element in the pathway is a final_state,
+        # indicated by None as the index of the agent
+        if history[-1][3] is not None:
+            raise Exception("The last episode pathway element had a non-None agent_ind, indicating the episode is not yet done.")
+
+        # Get agent and reward of the last action
+        last_agent_ind = history[-2][3]
+        last_reward = history[-2][2]
+
+        # If the last agent and the given agent are the same, return 0
+        if last_agent_ind == agent_ind:
+            return 0
+        
+        # Return the negative of the reward gained by the last agent
+        return -last_reward
             
 class Agent(ABC):
     '''
